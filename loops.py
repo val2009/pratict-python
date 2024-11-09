fcd3b59cfdb2e0d0c301a6ef1379695451642554
Valentin <assognavalentin@gmail.com>
2024-11-09 12:17:29 +0100

    quiz

---
+++ loops.py
@@ -0,0 +1,5 @@
+ins = [1, 2, 3, 4, 5, 6, 7, 8, 9,]
+for ciao in ins:
+    
+    print(ciao)
+print("FINISH")   
--- test.py
+++ test.py
@@ -1,2 +1,30 @@
-print("hello world")
-print("valentin trop bg")
+import random
+print("Quiz de culture G")
+presse = input("Presser [x] pour commencer\n")
+questions_list = [1,2]
+if presse == "x" or presse== "X" :
+    for question in questions_list:
+       randomquestion = random.choice(questions_list) 
+       if randomquestion == 1:
+            print("1ère Question")
+            print("La capital de Chypre est:")
+            print("[a] Nicosie")
+            print("[b] La Valette")
+            print("[c] Dublin")
+            if input() == "a":
+                print("Ta reponse est correcte")
+            else:
+                print("Ta reponse n'est pas correct")
+            questions_list.remove(randomquestion)
+       if randomquestion == 2: 
+            print("1ère Question")
+            print("La capital de l italie est:")
+            print("[a] Nicosie")
+            print("[b] La Valette")
+            print("[c] Dublin") 
+            if input() == "a":
+                print("Ta reponse est correcte")
+
+            else:
+                print("Ta reponse n'est pas correct")        
+            questions_list.remove(randomquestion)
